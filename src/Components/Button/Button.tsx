1021b1bf00b56f2493e52a8a89877f1f48ae79d6
Aldo <xhafajaldo16@gmail.com>
2024-03-19 11:20:56 +0100

    pipeline checks and format code prettier fix

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -5,45 +5,45 @@ import { Color } from "@themes/color";
 import { FontSize } from "@themes/fontSize";
 
 export const Button = ({
-	children,
-	className,
-	variant,
-	backgroundColor = Color.GREY_1,
-	size = "sm",
-	radius = "sm",
-	spinnerPlacement = "start",
-	startContent,
-	endContent,
-	spinner,
-	isIconOnly = false,
-	isDisabled = false,
-	isLoading = false,
-	onPress,
-	height = VerticalSpacing.M_L,
-	fontSize = FontSize.OVERLINE,
+  children,
+  className,
+  variant,
+  backgroundColor = Color.GREY_1,
+  size = "sm",
+  radius = "sm",
+  spinnerPlacement = "start",
+  startContent,
+  endContent,
+  spinner,
+  isIconOnly = false,
+  isDisabled = false,
+  isLoading = false,
+  onPress,
+  height = VerticalSpacing.M_L,
+  fontSize = FontSize.OVERLINE,
 }: ButtonComponentProps) => {
-	return (
-		<ButtonComponent
-			className={className}
-			variant={variant}
-			size={size}
-			radius={radius}
-			spinnerPlacement={spinnerPlacement}
-			startContent={startContent}
-			endContent={endContent}
-			spinner={spinner}
-			isIconOnly={isIconOnly}
-			isDisabled={isDisabled}
-			isLoading={isLoading}
-			fullWidth
-			style={{
-				backgroundColor,
-				height,
-				fontSize,
-			}}
-			onPress={onPress}
-		>
-			{children}
-		</ButtonComponent>
-	);
+  return (
+    <ButtonComponent
+      className={className}
+      variant={variant}
+      size={size}
+      radius={radius}
+      spinnerPlacement={spinnerPlacement}
+      startContent={startContent}
+      endContent={endContent}
+      spinner={spinner}
+      isIconOnly={isIconOnly}
+      isDisabled={isDisabled}
+      isLoading={isLoading}
+      fullWidth
+      style={{
+        backgroundColor,
+        height,
+        fontSize,
+      }}
+      onPress={onPress}
+    >
+      {children}
+    </ButtonComponent>
+  );
 };
--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -2,39 +2,39 @@ import { Input as InputComponent } from "@nextui-org/react";
 import { InputComponentProps } from "./types";
 
 export const Input = ({
-	size = "sm",
-	type,
-	label,
-	variant = "flat",
-	value,
-	defaultValue = "",
-	startContent,
-	endContent,
-	errorMessage,
-	isInvalid = false,
-	isDisabled = false,
-	isReadOnly = false,
-	isRequired = false,
-	onChange,
+  size = "sm",
+  type,
+  label,
+  variant = "flat",
+  value,
+  defaultValue = "",
+  startContent,
+  endContent,
+  errorMessage,
+  isInvalid = false,
+  isDisabled = false,
+  isReadOnly = false,
+  isRequired = false,
+  onChange,
 }: InputComponentProps) => {
-	return (
-		<InputComponent
-			size={size}
-			type={type}
-			label={label}
-			value={value}
-			defaultValue={defaultValue}
-			variant={variant}
-			startContent={startContent}
-			endContent={endContent}
-			isInvalid={isInvalid}
-			errorMessage={errorMessage}
-			isDisabled={isDisabled}
-			isReadOnly={isReadOnly}
-			isRequired={isRequired}
-			onChange={(event) => onChange(event.target.value)}
-			autoComplete="on"
-			fullWidth
-		/>
-	);
+  return (
+    <InputComponent
+      size={size}
+      type={type}
+      label={label}
+      value={value}
+      defaultValue={defaultValue}
+      variant={variant}
+      startContent={startContent}
+      endContent={endContent}
+      isInvalid={isInvalid}
+      errorMessage={errorMessage}
+      isDisabled={isDisabled}
+      isReadOnly={isReadOnly}
+      isRequired={isRequired}
+      onChange={(event) => onChange(event.target.value)}
+      autoComplete="on"
+      fullWidth
+    />
+  );
 };

07f02ecf19168e2c12142e13a827961c20a176c6
Aldo <xhafajaldo16@gmail.com>
2024-03-20 15:57:18 +0100

    created input and button stories with controls

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -4,46 +4,50 @@ import { VerticalSpacing } from "@themes/spacing";
 import { Color } from "@themes/color";
 import { FontSize } from "@themes/fontSize";
 
+const BUTTON_MAX_HEIGHT = 40;
+const BUTTON_MIN_HEIGHT = 20;
 export const Button = ({
-  children,
-  className,
-  variant,
-  backgroundColor = Color.GREY_1,
-  size = "sm",
-  radius = "sm",
-  spinnerPlacement = "start",
-  startContent,
-  endContent,
-  spinner,
-  isIconOnly = false,
-  isDisabled = false,
-  isLoading = false,
-  onPress,
-  height = VerticalSpacing.M_L,
-  fontSize = FontSize.OVERLINE,
+	children,
+	className,
+	variant,
+	backgroundColor = Color.GREY_1,
+	size = "sm",
+	radius = "sm",
+	spinnerPlacement = "start",
+	startContent,
+	endContent,
+	spinner,
+	isIconOnly = false,
+	isDisabled = false,
+	isLoading = false,
+	onPress,
+	height = VerticalSpacing.M_L,
+	fontSize = FontSize.OVERLINE,
 }: ButtonComponentProps) => {
-  return (
-    <ButtonComponent
-      className={className}
-      variant={variant}
-      size={size}
-      radius={radius}
-      spinnerPlacement={spinnerPlacement}
-      startContent={startContent}
-      endContent={endContent}
-      spinner={spinner}
-      isIconOnly={isIconOnly}
-      isDisabled={isDisabled}
-      isLoading={isLoading}
-      fullWidth
-      style={{
-        backgroundColor,
-        height,
-        fontSize,
-      }}
-      onPress={onPress}
-    >
-      {children}
-    </ButtonComponent>
-  );
+	return (
+		<ButtonComponent
+			className={className}
+			variant={variant}
+			size={size}
+			radius={radius}
+			spinnerPlacement={spinnerPlacement}
+			startContent={startContent}
+			endContent={endContent}
+			spinner={spinner}
+			isIconOnly={isIconOnly}
+			isDisabled={isDisabled}
+			isLoading={isLoading}
+			fullWidth
+			style={{
+				backgroundColor,
+				height,
+				fontSize,
+				maxHeight: BUTTON_MAX_HEIGHT,
+				minHeight: BUTTON_MIN_HEIGHT,
+			}}
+			onPress={onPress}
+		>
+			{children}
+		</ButtonComponent>
+	);
 };
